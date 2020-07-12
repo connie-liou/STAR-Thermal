d1faeca5bf66dbb0e526b5626a104869af8bc36e
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2020-07-12 14:36:14 -0400

    Added additional unit tests for Node class
    Tested getBlackbodyEmissivePower() and getSurfaceResistance() methods

--- test/testNode.py
+++ test/testNode.py
@@ -4,9 +4,10 @@ from Node import Node
 from OpticalProperties import OpticalProperties
 from PhysicalProperties import PhysicalProperties
 
+startTemperature = 300
 physicalProperties = PhysicalProperties(3, 4, 5, 6)
 opticalProperties = OpticalProperties(0.3, 0.4)
-node = Node("node1", physicalProperties, opticalProperties, 300)
+node = Node("node1", physicalProperties, opticalProperties, startTemperature)
 
 
 class TestNode(unittest.TestCase):
@@ -15,6 +16,12 @@ class TestNode(unittest.TestCase):
 
     def testNodeOpticalProperties(self) -> None:
         self.assertEqual(opticalProperties, node.opticalProperties)
+
+    def testBlackbodyEmissivePower(self) -> None:
+        self.assertAlmostEqual(459.27, node.getBlackbodyEmissivePower())
+
+    def testSurfaceResistance(self) -> None:
+        self.assertAlmostEqual(0.5, node.getSurfaceResistance())
 
 
 if __name__ == "__main__":
