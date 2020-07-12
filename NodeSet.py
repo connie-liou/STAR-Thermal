c0c572a969e72242f62b9e2ca5e1df5600861487
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2020-07-12 10:54:41 -0400

    Can get radiative heat transfer rates for NodeSet

--- NodeSet.py
+++ NodeSet.py
@@ -5,6 +5,7 @@ import numpy as np
 from Node import Node
 
 
+# TODO: Calculate radiation view factors using formulas
 class NodeSet:
     def __init__(
         self,
@@ -17,6 +18,42 @@ class NodeSet:
         self.contactConductanceMatrix = contactConductanceMatrix
         self.numNodes = len(nodes)
 
+    def getSpaceResistance(self, sendNodeIndex: int, receiveNodeIndex: int) -> float:
+        sendNode = self.nodes[sendNodeIndex]
+        receiveNode = self.nodes[receiveNodeIndex]
+        viewFactor = self.radiationViewFactorMatrix[sendNodeIndex][receiveNodeIndex]
+        return _getSpaceResistance(sendNode, receiveNode, viewFactor)
+
+    def _getRadiationHeatTransferSystemCoefficientsSingleNode(
+        self, nodeIndex: int
+    ) -> np.array:
+        node = self.nodes[nodeIndex]
+        result = np.zeros((1, self.numNodes))
+        result[nodeIndex] = -1 / node.getSurfaceResistance()
+        for j in range(self.numNodes):
+            spaceResistance = self.getSpaceResistance(nodeIndex, j)
+            result[j] += 1 / spaceResistance
+            result[nodeIndex] -= 1 / spaceResistance
+        return result
+
+    def _getRadiationHeatTransferSystemCoefficients(self) -> np.ndarray:
+        coefficients = []
+        for nodeIndex in range(self.numNodes):
+            coefficients.append(
+                self._getRadiationHeatTransferSystemCoefficientsSingleNode(nodeIndex)
+            )
+        return np.array(coefficients)
+
+    def _getRadiationHeatTransferSystemDependentVariables(self) -> np.array:
+        return np.array(
+            map(_getRadiationHeatTransferSystemDependentVariable, self.nodes)
+        )
+
+    def getRadiationHeatTransferRates(self) -> np.array:
+        coefficients = self._getRadiationHeatTransferSystemCoefficients()
+        dependentVariables = self._getRadiationHeatTransferSystemDependentVariables()
+        return np.linalg.solve(coefficients, dependentVariables)
+
     def getConductionRateMatrix(self) -> np.ndarray:
         conductionMatrix = np.zeros((self.numNodes, self.numNodes))
         for i in range(self.numNodes):
@@ -24,7 +61,7 @@ class NodeSet:
                 sendNode = self.nodes[i]
                 receiveNode = self.nodes[j]
                 contactConductance = self.contactConductanceMatrix[i][j]
-                conduction = getConductionRate(
+                conduction = _getConductionRate(
                     sendNode, receiveNode, contactConductance
                 )
                 conductionMatrix[i][j] = conduction
@@ -34,7 +71,7 @@ class NodeSet:
 
 def validateRadiationViewFactorMatrix(
     nodes: List[Node], radiationViewFactorMatrix: np.ndarray
-) -> bool:  # TODO: Calculate radiation view factors using formulas
+) -> bool:
     surfaceAreas = list(map(lambda node: node.physicalProperties.surfaceArea, nodes))
     numNodes = len(nodes)
     for i in range(numNodes):
@@ -46,8 +83,16 @@ def validateRadiationViewFactorMatrix(
     return True
 
 
-def getConductionRate(
+def _getConductionRate(
     sendNode: Node, receiveNode: Node, contactConductance: float
 ) -> float:
     temperatureDifference = sendNode.temperature - receiveNode.temperature
     return contactConductance * temperatureDifference
+
+
+def _getSpaceResistance(sendNode: Node, receiveNode: Node, viewFactor: float) -> float:
+    return 1 / (sendNode.physicalProperties.surfaceArea * viewFactor)
+
+
+def _getRadiationHeatTransferSystemDependentVariable(node: Node) -> float:
+    return -node.getBlackbodyEmissivePower() / node.getSurfaceResistance()
