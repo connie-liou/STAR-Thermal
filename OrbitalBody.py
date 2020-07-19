efff3878260b9311a79898a9a4b52824a6e26fba
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2020-07-19 13:35:16 -0400

    Made OrbitalBody instance for Earth
    Added needed constants in constants.py.

--- OrbitalBody.py
+++ OrbitalBody.py
@@ -1,14 +1,26 @@
+import constants
+
+
 class OrbitalBody:
     def __init__(
         self,
         radius: float,
         gravitationalParamter: float,
-        effectiveBlackbodyTemperature: float,
+        effectiveTemperature: float,
         albedo: float,
         solarConstant: float,
     ):
         self.radius = radius
-        self.gravitationalParamter = (gravitationalParamter,)
-        self.effectiveBlackbodyTemperature = (effectiveBlackbodyTemperature,)
-        self.albedo = (albedo,)
+        self.gravitationalParamter = gravitationalParamter
+        self.effectiveTemperature = effectiveTemperature
+        self.albedo = albedo
         self.solarConstant = solarConstant
+
+
+EARTH_ORBITAL_BODY = OrbitalBody(
+    constants.EARTH_RADIUS,
+    constants.EARTH_GRAVITATIONAL_PARAMETER,
+    constants.EARTH_EFFECTIVE_TEMPERATURE,
+    constants.EARTH_ALBEDO,
+    constants.EARTH_SOLAR_CONSTANT,
+)
