ce2a7d677a9b1a1f730569e064f90ff386d8ece4
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2020-07-19 14:11:46 -0400

    Added getEccentricAnomaly method to Orbit

--- Orbit.py
+++ Orbit.py
@@ -1,3 +1,7 @@
+from math import sin, sqrt
+
+from scipy.optimize import fsolve
+
 from OrbitalBody import OrbitalBody
 
 
@@ -17,3 +21,22 @@ class Orbit:
         self.maxAltitude = maxAltitude
         self.eccentricity = eccentricity
         self.orbitalBody = orbitalBody
+        self.semiMajorAxis = _getSemiMajorAxis(self)
+
+    def getEccentricAnomaly(self, time: float) -> float:
+        return _getEccentricAnomaly(self, time)
+
+
+def _getSemiMajorAxis(orbit: Orbit) -> float:
+    return (orbit.orbitalBody.radius + orbit.maxAltitude) / (1 + orbit.eccentricity)
+
+
+def _getEccentricAnomaly(orbit: Orbit, time: float) -> float:
+    coefficient = sqrt(
+        pow(orbit.semiMajorAxis, 3) / orbit.orbitalBody.gravitationalParamter
+    )
+    function = lambda E: coefficient * (E - orbit.eccentricity * sin(E)) - time
+    # The estimated root is obtained by using the approximation sin(E) ~ E.
+    estimatedRoot = time / (coefficient * (1 - orbit.eccentricity))
+    root: float = fsolve(function, estimatedRoot)[0]
+    return root
