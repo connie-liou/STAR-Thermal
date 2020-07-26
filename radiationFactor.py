213421c83c0faf8100ef234559316f9f6f2d1b7e
mja224 <65874507+mja224@users.noreply.github.com>
2020-07-26 17:19:22 -0400

    Update radiationFactor.py

--- radiationFactor.py
+++ radiationFactor.py
@@ -2,13 +2,13 @@ import numpy, math
 import scipy as sc
 from scipy import integrate
 from scipy.integrate import quad, dblquad, nquad
-def radiationFunction(areai, thetai, thetaj, sD, areaj):
-    #sD = shortestDist
+def radiationFunction(thetai, thetaj, sD):
+    #sD = shortest distance
     return cos(thetai)*cos(thetaj)/(Pi*sD*sD)
 
-def bounds_i():
+def bounds_i(areai):
     return [0, areai]
-def bounds_j():
+def bounds_j(areaj):
     return [0, areaj]
 
 radiationIntegral = integrate.nquad(radiationFunction, [bounds_i, bounds_j])
@@ -16,4 +16,6 @@ radiationIntegral = integrate.nquad(radiationFunction, [bounds_i, bounds_j])
 radiationFactor = 1/(areai) * radiationIntegral
 print(radiationFactor)
 
-radiationFunction(1, 2, 3, 4, 1)
+radiationFunction(2, 3, 4)
+bounds_i(1)
+bounds_j(1)
