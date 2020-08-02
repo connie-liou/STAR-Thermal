1d33666756627cd21598a8cf033ff3780daabf7e
mja224 <65874507+mja224@users.noreply.github.com>
2020-08-02 19:12:03 -0400

    Update EclipseAngle.py

--- EclipseAngle.py
+++ EclipseAngle.py
@@ -4,33 +4,35 @@ import math
 #determine if cubesat is in eclipse
 #Re = radius of Earth in m
 Re = 6371000
+def eclipseAngle(scx, scy, scz, sx, sy, sz):
+    #find magnitude of spacecraft position vector
+    #vector found from previous file code 2.2.1
+    SpacecraftPosition = np.array([scx, scy, scz])
+    Rsc = np.linalg.norm(SpacecraftPosition)
+    print('Rsc', Rsc)
 
-#find magnitude of spacecraft position vector
-#vector found from previous file code 2.2.1
-SpacecraftPosition = np.array([scx, scy, scz])
-Rsc = np.linalg.norm(SpacecraftPosition)
-print('Rsc', Rsc)
+    #find magnitude of sun position vector
+    SunPosition = np.array([sx, sy, sz])
+    Rs = np.linalg.norm(SunPosition)
+    print('Rs', Rs)
 
-#find magnitude of sun position vector
-SunPosition = np.array([sx, sy, sz])
-Rs = np.linalg.norm(SunPosition)
-print('Rs', Rs)
+    #calculate the two angles that define the transition point from sun to shade
+    input1 = Re/Rsc
+    input2 = Re/Rs
 
-#calculate the two angles that define the transition point from sun to shade
-input1 = Re/Rsc
-input2 = Re/Rs
+    theta1 = math.acos(input1)
+    theta2 = math.acos(input2)
 
-theta1 = math.acos(input1)
-theta2 = math.acos(input2)
+    thetaTotal = theta1 + theta2
 
-thetaTotal = theta1 + theta2
-
-#calculate the angle between the spacecraft's actual position vector and the sun's position vector
-thetaActual = math.acos((np.dot(SpacecraftPosition, SunPosition))/(Rsc*Rs))
-print(thetaTotal, thetaActual)
+    #calculate the angle between the spacecraft's actual position vector and the sun's position vector
+    thetaActual = math.acos((np.dot(SpacecraftPosition, SunPosition))/(Rsc*Rs))
+    print(thetaTotal, thetaActual)
 
 
-if thetaActual < thetaTotal:
-    print('line of sight does exist and the spacecraft is in sunlight')
-else:
-    print('there is no line of sight between the Sun and the spacecraft and the spacecraft is in eclipse')
+    if thetaActual < thetaTotal:
+        print('line of sight does exist and the spacecraft is in sunlight')
+    else:
+        print('there is no line of sight between the Sun and the spacecraft and the spacecraft is in eclipse')
+
+eclipseAngle(2, 3, 4, 2, 5, 6)
