dcdee5050664687518afdcd1228c424cae22c617
Connieliou57 <connieliou57@gmail.com>
2020-08-02 04:17:09 -0400

    function updates
    for trueanomaly and radiationfactor integral

--- radiationFactor.py
+++ radiationFactor.py
@@ -6,35 +6,25 @@ from scipy.integrate import quad, dblquad, nquad
 
 import numpy as np
 
+def integrateRadiationFactors(width_i, width_j, length_i, length_j, thetai, thetaj):
 
-def radiationFunction(thetai, thetaj, width_i, width_j):
-     return cos(thetai)*cos(thetaj)/(np.pi*((width_i/2)**2+(width_j/2)**2))
+    def radiationFunction(thetai, thetaj, width_i, width_j):
+        return np.cos(thetai)*np.cos(thetaj)/(np.pi*((width_i/2)**2+(width_j/2)**2))
 
-def bounds_lengthi(length_i):
-    return [0, length_i]
-def bounds_widthi(width_i):
-    return [0, width_i]
+    def bounds_lengthi():
+        return [0, length_i]
+    def bounds_widthi():
+        return [0, width_i]
 
-def bounds_lengthj(length_j):
-    return [0, length_j]
-def bounds_widthj(width_j):
-    return [0, width_j]
+    def bounds_lengthj():
+        return [0, length_j]
+    def bounds_widthj():
+        return [0, width_j]
 
-def area_i(length_i, width_i):
-    return length_i*width_i
+    area_i = length_i*width_i
 
-radiationIntegral = integrate.nquad(radiationFunction, [bounds_lengthi, bounds_widthi, bounds_lengthj, bounds_widthj])
+    radiationIntegral = integrate.nquad(radiationFunction, [bounds_lengthi, bounds_widthi, bounds_lengthj, bounds_widthj])
 
-radiationFactor = 1/(area_i) * radiationIntegral
+    return (1/(area_i) * radiationIntegral)
 
-
-radiationFunction(2, 3, 4, 4)
-bounds_lengthi(2)
-bounds_widthi(3)
-bounds_lengthj(4)
-bounds_widthj(2)
-area_i(2, 3)
-
-print(radiationIntegral)
-print(radiationFactor)
-
+print(integrateRadiationFactors(1, 1, 1, 1, .5, .5))
