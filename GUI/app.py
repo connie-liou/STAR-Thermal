c9b87030226d6e6b50c68ef529a13b68d11aefc1
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2021-02-21 19:09:32 -0500

    Add simple callback

--- GUI/app.py
+++ GUI/app.py
@@ -12,10 +12,13 @@ import numpy as np
 from HelpPopups import popupCallbacks
 from Layout import layout
 
+print('Running app.py')
 
 '''
 Init dash and flask applications and add extra assests
 '''
+
+
 def find_data_file(filename):
     if getattr(sys, 'frozen', False):
         # The application is frozen
@@ -26,6 +29,8 @@ def find_data_file(filename):
         datadir = os.path.dirname(__file__)
 
     return os.path.join(datadir, filename)
+
+
 server = Flask('app')
 
 app = dash.Dash(__name__, assets_folder=find_data_file('assets/'), server=server)
@@ -33,91 +38,105 @@ app = dash.Dash(__name__, assets_folder=find_data_file('assets/'), server=server
 app.title = 'EPS Design Tool'
 
 layout(app)
-#CallBacks #####################################################################################################
-@app.callback([Output('node-table', 'columns'), Output('radiation-table', 'columns'), 
-            Output('radiation-table','data'), Output('conductance-table', 'columns'), 
-            Output('conductance-table', 'data'), Output('prev_num_nodes', 'children'),
-            Output('rad-matrix-error', 'style'), Output('cond-matrix-error', 'style')], 
-            [Input('num-nodes', 'value'), Input('radiation-table','data_timestamp'), Input('conductance-table','data_timestamp')], 
-            [State('node-table','columns'), State('radiation-table', 'columns'), 
-            State('radiation-table','data'), State('conductance-table', 'columns'), 
-            State('conductance-table', 'data'),State('prev_num_nodes', 'children')])
-def addNodes(numNodes, radActive, condActive, nodeCols, radCols, radRows, condCols, condRows,prevNumNodes):
+
+
+# CallBacks #####################################################################################################
+@app.callback(
+    [Output(component_id='simple-orbit-container', component_property='style')],
+    [Input('run_button', 'n_clicks')]
+)
+def printSomething(clicks):
+    print("This printed something")
+    print(f'clicks: {clicks}')
+    return [{'display': 'none'}]
+
+
+@app.callback([Output('node-table', 'columns'), Output('radiation-table', 'columns'),
+               Output('radiation-table', 'data'), Output('conductance-table', 'columns'),
+               Output('conductance-table', 'data'), Output('prev_num_nodes', 'children'),
+               Output('rad-matrix-error', 'style'), Output('cond-matrix-error', 'style')],
+              [Input('num-nodes', 'value'), Input('radiation-table', 'data_timestamp'),
+               Input('conductance-table', 'data_timestamp')],
+              [State('node-table', 'columns'), State('radiation-table', 'columns'),
+               State('radiation-table', 'data'), State('conductance-table', 'columns'),
+               State('conductance-table', 'data'), State('prev_num_nodes', 'children')])
+def addNodes(numNodes, radActive, condActive, nodeCols, radCols, radRows, condCols, condRows, prevNumNodes):
     if prevNumNodes is None:
         prevNumNodes = 0
-    
-    if(numNodes == prevNumNodes):
-        rad_df= pd.DataFrame(radRows)
+
+    if (numNodes == prevNumNodes):
+        rad_df = pd.DataFrame(radRows)
         rad_arr = rad_df.values[:, 1:]
         isRadSymm = True
         # print(rad_arr)
 
-        cond_df= pd.DataFrame(condRows)
+        cond_df = pd.DataFrame(condRows)
         cond_arr = cond_df.values[:, 1:]
         isCondSymm = True
         # print(cond_arr)
-        
 
-        radStyle = {'display':'none'}
-        condStyle = {'display':'none'}
+        radStyle = {'display': 'none'}
+        condStyle = {'display': 'none'}
 
-        for i in range (0, numNodes):
-            for j in range (0, numNodes):
+        for i in range(0, numNodes):
+            for j in range(0, numNodes):
                 if i == j:
-                    if rad_arr[i][j]!=0:
+                    if rad_arr[i][j] != 0:
                         isRadSymm = False
-                        radStyle = {'display':'block'}
-                    if cond_arr[i][j]!=0:
+                        radStyle = {'display': 'block'}
+                    if cond_arr[i][j] != 0:
                         isCondSymm = False
-                        condStyle = {'display':'block'}
-                else: 
+                        condStyle = {'display': 'block'}
+                else:
                     if rad_arr[i][j] != rad_arr[j][i]:
                         isRadSymm = False
-                        radStyle = {'display':'block'}
+                        radStyle = {'display': 'block'}
                     if cond_arr[i][j] != cond_arr[j][i]:
                         isCondSymm = False
-                        condStyle = {'display':'block'}
+                        condStyle = {'display': 'block'}
         # print(isRadSymm, isCondSymm)
         # print('----')
         return [nodeCols, radCols, radRows, condCols, condRows, numNodes, radStyle, condStyle]
-    else:        
+    else:
         if numNodes >= len(nodeCols):
             extra = numNodes - len(nodeCols) + 1
-            for i in range (extra):
+            for i in range(extra):
                 nodeCols.append({
-                            'id': ('node-'+str(len(nodeCols))), 'name': ('Node '+str(len(nodeCols))),
-                            'renamable': True, 'deletable': False, 'editable':True, 'type': 'numeric'
-                            })
+                    'id': ('node-' + str(len(nodeCols))), 'name': ('Node ' + str(len(nodeCols))),
+                    'renamable': True, 'deletable': False, 'editable': True, 'type': 'numeric'
+                })
                 radCols.append({
-                            'id': ('radiation-'+str(len(nodeCols)-1)), 'name': ('Surface '+str(len(nodeCols)-1)),
-                            'renamable': True, 'deletable': False, 'editable':True, 'type': 'numeric'
-                            })
-                newRow = {**{radCols[0]['id']:('Surface ' + str(len(nodeCols)-1))},** {i['id']:0 for i in radCols[1:]}}
+                    'id': ('radiation-' + str(len(nodeCols) - 1)), 'name': ('Surface ' + str(len(nodeCols) - 1)),
+                    'renamable': True, 'deletable': False, 'editable': True, 'type': 'numeric'
+                })
+                newRow = {**{radCols[0]['id']: ('Surface ' + str(len(nodeCols) - 1))},
+                          **{i['id']: 0 for i in radCols[1:]}}
                 radRows.append(newRow)
                 condCols.append({
-                            'id': ('conductance-'+str(len(nodeCols)-1)), 'name': ('Surface '+str(len(nodeCols)-1)),
-                            'renamable': True, 'deletable': False, 'editable':True, 'type': 'numeric'
-                            })
-                newRow = {**{condCols[0]['id']:('Surface ' + str(len(nodeCols)-1))}, **{i['id']:0 for i in condCols[1:]}}
+                    'id': ('conductance-' + str(len(nodeCols) - 1)), 'name': ('Surface ' + str(len(nodeCols) - 1)),
+                    'renamable': True, 'deletable': False, 'editable': True, 'type': 'numeric'
+                })
+                newRow = {**{condCols[0]['id']: ('Surface ' + str(len(nodeCols) - 1))},
+                          **{i['id']: 0 for i in condCols[1:]}}
                 condRows.append(newRow)
-        elif numNodes < len(nodeCols)-1:
+        elif numNodes < len(nodeCols) - 1:
             nodeCols.pop()
             radCols.pop()
             radRows.pop()
             condCols.pop()
             condRows.pop()
 
-
         return [nodeCols, radCols, radRows, condCols, condRows, numNodes, dash.no_update, dash.no_update]
 
-# @app.callback([Output('radiation-table', 'style-cell'), Output('conductance-table', 'style-cell')], 
-#             [Input('radiation-table','data'), Input('conductance-table', 'data')], 
+
+# @app.callback([Output('radiation-table', 'style-cell'), Output('conductance-table', 'style-cell')],
+#             [Input('radiation-table','data'), Input('conductance-table', 'data')],
 #             [State('num-nodes', 'value'),State('radiation-table', 'columns'), State('conductance-table', 'columns')])
 # def addNodes(radData, condData, numNodes, radCols, condCols):
-    
 
 
-@app.callback(Output('confirm-zerodivision-error', 'displayed'), [Input('confirm-zerodivision-error', 'message')], [State('run_button', 'n_clicks'), State('prev_clicks_run', 'children')])
+@app.callback(Output('confirm-zerodivision-error', 'displayed'), [Input('confirm-zerodivision-error', 'message')],
+              [State('run_button', 'n_clicks'), State('prev_clicks_run', 'children')])
 def display_confirm(value, clicks, prevClicks):
     if clicks is None:
         clicks = 0
@@ -128,23 +147,26 @@ def display_confirm(value, clicks, prevClicks):
     else:
         return True
 
-@app.callback([Output('all-tab-container', 'className'), Output('show-container', 'style'), Output('hide-container', 'style')],
-            [Input('hide-tabs', 'n_clicks'), Input('show-tabs', 'n_clicks')])
-def hideTabs (hideClicks, showClicks):
+
+@app.callback(
+    [Output('all-tab-container', 'className'), Output('show-container', 'style'), Output('hide-container', 'style')],
+    [Input('hide-tabs', 'n_clicks'), Input('show-tabs', 'n_clicks')])
+def hideTabs(hideClicks, showClicks):
     if hideClicks is None:
         hideClicks = 0
     if showClicks is None:
         showClicks = 0
     if showClicks == 0 and showClicks == hideClicks:
         return ['pretty_container fadein', {'display': 'none'}, {'display': 'flex'}]
-    elif showClicks>=hideClicks:
+    elif showClicks >= hideClicks:
         return ['pretty_container fadein', {'display': 'none'}, {'display': 'flex'}]
     else:
         return ['pretty_container fadein fadeout', {'display': 'flex'}, {'display': 'none'}]
 
-@app.callback( 
+
+@app.callback(
     [Output(component_id='LEO-orbit-container', component_property='style'),
-    Output(component_id='constant-orbit-container', component_property='style')],
+     Output(component_id='constant-orbit-container', component_property='style')],
     [Input('orbit-type', 'value')])
 def change_orbit(orbit):
     if (orbit == 'L1' or orbit == 'L2'):
@@ -152,6 +174,7 @@ def change_orbit(orbit):
 
     else:
         return {'display': 'block'}, {'display': 'none'}
+
 
 @app.callback(
     [Output(component_id='simple-orbit-container', component_property='style'),
@@ -162,7 +185,7 @@ def use_simple_orbit(orbitType):
         return {'display': 'block'}, {'display': 'none'}
     else:
         return {'display': 'none'}, {'display': 'block'}
-        
+
 popupCallbacks(app)
 if __name__ == '__main__':
     app.run_server(debug=True, port=8050)
--- GUI/thermalLoop.py
+++ GUI/thermalLoop.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append("/model/")
-from thermalModel import ThermalModel
+# from thermalModel import ThermalModel
 import dash
 from dash.dependencies import *
-import dash_html_components as html 
+import dash_html_components as html
 from dash.exceptions import PreventUpdate
 
 from model.components.nodeSet import NodeSet
@@ -14,45 +14,46 @@ from model.components.solarPosition import SolarPosition
 from model.components.spacecraftConfiguration import SpacecraftConfiguration
 from model.components.timingConfiguration import TimingConfiguration
 
+
 def thermalLoop(app):
     @app.callback(
         [Output('thermal-graph', 'figure')],
         [Input('run_button', 'n_clicks'),
-        Input('num-nodes','value'),
-        Input('node-table','data'),
-        Input('node-table','columns'),
-        Input('radiation-table','data'),
-        Input('radiation-table','columns'),
-        Input('conductance-table','data'),
-        Input('conductance-table','columns'),
-        Input('use-simple-orbit','value'),
-        Input('orbit-type','value'),
-        Input('start-time','value'),
-        Input('end-time','value'),
-        Input('timestep','value'),
-        Input('period','value'),
-        Input('eclipse-length','value'),
-        Input('eclipse-start','value'),
-        Input('eclipse-end','value'), #complex orbit
-        Input('start-time-complex','value'),
-        Input('end-time-complex','value'),
-        Input('timestep-complex','value'),
-        Input('spacecraft-config','value'),
-        Input('inclination','value'),
-        Input('right-ascension','value'),
-        Input('periapsis','value'),
-        Input('max-altitude','value'),
-        Input('eccentricity','value'),
-        Input('z-initial-orient','value'),
-        Input('spacecraft-axis','value'),
-        Input('additional-rotation','value'),
-        Input('orbit-body-radius','value'),
-        Input('grav-param','value'),
-        Input('blackbody-temp','value'),
-        Input('orbiting-albedo','value'),
-        Input('solar-const-atm','value'),
-        Input('right-ascension-sun','value'),
-        Input('declination-sun','value')]
+         Input('num-nodes', 'value'),
+         Input('node-table', 'data'),
+         Input('node-table', 'columns'),
+         Input('radiation-table', 'data'),
+         Input('radiation-table', 'columns'),
+         Input('conductance-table', 'data'),
+         Input('conductance-table', 'columns'),
+         Input('use-simple-orbit', 'value'),
+         Input('orbit-type', 'value'),
+         Input('start-time', 'value'),
+         Input('end-time', 'value'),
+         Input('timestep', 'value'),
+         Input('period', 'value'),
+         Input('eclipse-length', 'value'),
+         Input('eclipse-start', 'value'),
+         Input('eclipse-end','value'),  #complex orbit
+         Input('start-time-complex','value'),
+         Input('end-time-complex','value'),
+         Input('timestep-complex','value'),
+         Input('spacecraft-config','value'),
+         Input('inclination','value'),
+         Input('right-ascension','value'),
+         Input('periapsis','value'),
+         Input('max-altitude','value'),
+         Input('eccentricity','value'),
+         Input('z-initial-orient','value'),
+         Input('spacecraft-axis','value'),
+         Input('additional-rotation','value'),
+         Input('orbit-body-radius','value'),
+         Input('grav-param','value'),
+         Input('blackbody-temp','value'),
+         Input('orbiting-albedo','value'),
+         Input('solar-const-atm','value'),
+         Input('right-ascension-sun','value'),
+         Input('declination-sun','value')]
     )
     def thermalLooping(clicks, num_nodes, node_table_rows, node_table_cols, radiation_table_rows, radiation_table_cols, conductance_table_rows,
             conductance_table_cols, use_simple_orbit, orbit_type, start_time, end_time, timestep, period, eclipse_length, eclipse_start, 
